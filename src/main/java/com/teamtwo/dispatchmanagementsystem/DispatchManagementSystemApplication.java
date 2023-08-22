c87abe04fee5b149229aab4ff9513588933c345c
jiezhongXiaoShawn <112424103+jiezhongXiaoShawn@users.noreply.github.com>
2023-08-22 15:36:40 -0500

    Update DispatchManagementSystemApplication.java

--- src/main/java/com/teamtwo/dispatchmanagementsystem/DispatchManagementSystemApplication.java
+++ src/main/java/com/teamtwo/dispatchmanagementsystem/DispatchManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DispatchManagementSystemApplication {
-
+//test
     public static void main(String[] args) {
         SpringApplication.run(DispatchManagementSystemApplication.class, args);
     }
