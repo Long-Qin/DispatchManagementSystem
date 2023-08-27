ce610eb4a414129e802c899cca74644d13aae12b
NahanahaF <54884798+NahanahaF@users.noreply.github.com>
2023-08-26 20:57:29 -0700

    Cors setup (#10)
    * Update TwoByTwo.java
    
    * register implement
    
    * register implement
    
    * Commit-DowngradedSpringBootVersion (#3)
    
    Co-authored-by: jiezhongXiaoShawn <112424103+jiezhongXiaoShawn@users.noreply.github.com>
    
    * token base implement
    
    * token base implement
    
    * CORS setup
    
    Allowing CORS request.
    
    ---------
    
    Co-authored-by: ZhangSan <jiaheng@houtoumeiyou.com>
    Co-authored-by: jiezhongXiaoShawn <112424103+jiezhongXiaoShawn@users.noreply.github.com>
    Co-authored-by: EricLu <138171374+glu4712@users.noreply.github.com>

--- src/main/java/com/teamtwo/dispatchmanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/teamtwo/dispatchmanagementsystem/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+
 
 
 import javax.sql.DataSource;
@@ -29,14 +34,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors()  // 1. 添加这一行来启用CORS
+                .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/register").permitAll()
                 .antMatchers(HttpMethod.POST, "/authenticate").permitAll()
                 .antMatchers("/orders/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .csrf()
-                .disable();
+                .csrf().disable();
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -51,6 +57,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));  // 允许的源
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // 允许的HTTP方法
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));  // 允许的请求头
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
